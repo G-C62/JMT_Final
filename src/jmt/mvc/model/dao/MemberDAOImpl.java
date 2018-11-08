8e96ea967a7c4ea1538687603e5ce93a395bbf79
kosta <kosta@192.168.0.130>
2018-11-08 13:09:52 +0900

    호수

--- src/jmt/mvc/model/dao/MemberDAOImpl.java
+++ src/jmt/mvc/model/dao/MemberDAOImpl.java
@@ -1,9 +1,14 @@
 package jmt.mvc.model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 
 import jmt.mvc.model.dto.MemberDTO;
+import jmt.mvc.model.util.DbUtil;
 
 public class MemberDAOImpl implements MemberDAO {
 
@@ -35,6 +40,25 @@ public class MemberDAOImpl implements MemberDAO {
 	public int update(MemberDTO memberDTO) {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	public static void selectBookmarkById(String id) throws SQLException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		int result = 0;
+		try {
+			con= DbUtil.getConnection();
+			ps = con.prepareStatement("SELECT RES_ID FROM BOOKMARK WHERE ID=?");
+			ps.setString(1, id);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			DbUtil.dbClose(rs, ps, con);
+		}
+		
 	}
 
 }
